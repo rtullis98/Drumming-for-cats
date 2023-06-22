a1a42e745c56784c83f1d6f14f00e0f58b21ff09
rtullis98 <121829715+rtullis98@users.noreply.github.com>
2023-06-21 19:54:13 -0500

    appointment form fixed, still can't see appointments

--- api/catData.js
+++ api/catData.js
@@ -16,6 +16,22 @@ const getCats = () => new Promise((resolve, reject) => {
     .catch((error) => reject(error));
 });
 
+// GET TEACHER CATS
+const getTeacherCat = () => new Promise((resolve, reject) => {
+  axios.get(`${dbUrl}/cats.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const teachers = Object.values(data).filter((item) => item.isTeacher);
+      resolve(teachers);
+    })
+    .catch(reject);
+});
+
 // CREATE CAT
 const createCat = (catObj) => new Promise((resolve, reject) => {
   axios.post(`${dbUrl}/cats.json`, catObj)
@@ -57,22 +73,6 @@ const getCatAppointments = (catFirebaseKey) => new Promise((resolve, reject) => 
   })
     .then((response) => response.json())
     .then((data) => resolve(Object.values(data)))
-    .catch(reject);
-});
-
-// GET TEACHER CATS
-const getTeacherCat = (firebaseKey) => new Promise((resolve, reject) => {
-  axios.get(`${dbUrl}/cats.json"${firebaseKey}"`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      const teachers = Object.values(data).filter((item) => item.isTeacher);
-      resolve(teachers);
-    })
     .catch(reject);
 });
 
